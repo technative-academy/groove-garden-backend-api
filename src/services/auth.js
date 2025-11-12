a41bca907c77e624124eacdfb4db22e004d4bc33
Fiarzen <jeremylam1995@gmail.com>
2025-11-12 11:30:01 +0000

    changed name to username

--- src/services/auth.js
+++ src/services/auth.js
@@ -15,7 +15,7 @@ const generateRefreshToken = (user) => {
   return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
 };
 
-const registerUser = async (name, email, password, bio) => {
+const registerUser = async (username, email, password, bio) => {
   // Check if the email already exists
   const emailCheckResult = await pool.query(
     "SELECT * FROM users WHERE email = $1",
@@ -29,8 +29,8 @@ const registerUser = async (name, email, password, bio) => {
   // hash the password and insert the new user into the database
   const hashedPassword = await bcrypt.hash(password, 10);
   const result = await pool.query(
-    "INSERT INTO users (name, email, password, bio) VALUES ($1, $2, $3, $4) RETURNING *",
-    [name, email, hashedPassword, bio]
+    "INSERT INTO users (username, email, password, bio) VALUES ($1, $2, $3, $4) RETURNING *",
+    [username, email, hashedPassword, bio]
   );
 
   // Return the newly created user
@@ -53,7 +53,7 @@ const loginUser = async (email, password) => {
 
     return {
       id: user.id,
-      name: user.name,
+      username: user.name,
       accessToken,
       refreshToken,
     };
