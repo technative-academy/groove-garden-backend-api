5b995b6e34fd91cf8662eadac8869c26d72edad8
Jeremy Lam <jeremylam1995@gmail.com>
2025-11-18 22:28:22 +0000

    removed my things from routes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import playlistRoutes from "./routes/playlists.js";
 import authRoutes from "./routes/auth.js";
 import usersRoutes from "./routes/users.js";
-import myThingsRoutes from "./routes/my-things.js";
 import songsRoutes from "./routes/songs.js";
 import artistsRoutes from "./routes/artists.js";
 import albumsRoutes from "./routes/albums.js";
