e8ff8120e181c9f4580a92f37d8889f6ad42e6c7
Fiarzen <jeremylam1995@gmail.com>
2025-11-26 09:07:50 +0000

    samesite to none' '

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,7 +29,7 @@ router.post("/login", async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
